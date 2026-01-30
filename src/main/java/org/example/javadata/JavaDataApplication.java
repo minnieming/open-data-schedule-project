722d827df9f00837a4698acd0ddf087d56257e1f
LeeEunmi <eunmilee9109.1@gmail.com>
2026-01-31 07:19:02 +0900

    시작 테스트

---
+++ src/main/java/org/example/javadata/JavaDataApplication.java
@@ -0,0 +1,14 @@
+package org.example.javadata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDataApplication.class, args);
+        System.out.println("시작하기");
+    }
+
+}
